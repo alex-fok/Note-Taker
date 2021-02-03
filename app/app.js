0455933f7cdbdf4c72a3504bd09e08216414f450
Alexander Fok <foktmalexander@gmail.com>
2021-02-02 21:44:28 -0800

    Change file directory

--- app/app.js
+++ app/app.js
@@ -17,11 +17,11 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
 app.post('/api/notes', (req,res) => {
